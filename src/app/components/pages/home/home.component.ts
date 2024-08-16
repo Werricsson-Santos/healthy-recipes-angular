4ca57584b0bb4dfe2566dcac5a53b71d2e47119d
Werricsson <werricsson.santos@hotmail.com>
2024-08-16 16:14:51 -0300

    :construction: Creating home page

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FoodCardComponent } from "./food-card/food-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FoodCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
