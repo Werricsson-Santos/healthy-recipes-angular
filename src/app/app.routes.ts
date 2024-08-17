837aec406ef0d6d91d440dd7a9649993541bb636
Werricsson <werricsson.santos@hotmail.com>
2024-08-17 11:54:40 -0300

    :construction: Rounting single-recipe page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { SearchComponent } from './components/pages/search/search.component';
+import { SingleRecipeComponent } from './components/pages/single-recipe/single-recipe.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'search', component: SearchComponent }
+    { path: 'search', component: SearchComponent },
+    { path: 'recipe', component: SingleRecipeComponent }
 ];
