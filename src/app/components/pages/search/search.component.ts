20f5601ccec1626e7a81717a66dc1e16f3fba6da
Werricsson <werricsson.santos@hotmail.com>
2024-08-17 14:57:47 -0300

    :construction: Creating search page

--- src/app/components/pages/search/search.component.ts
+++ src/app/components/pages/search/search.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { SearchService } from '../../../services/search/search.service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+
+export class SearchComponent  {
+  public loading = false;
+  public query: string = "";
+  public foods: any = [];
+
+  constructor(private service: SearchService) {}
+
+  getSearch() {
+    this.loading = true;
+    console.log("works!")
+    this.service.search(this.query).pipe(
+      finalize(() => {
+        this.loading = false;
+      })
+    ).subscribe((data: any) => this.foods = data.results);
+  }
+
+
+  onEnterKey(event: KeyboardEvent) {
+    if (event.key == 'Enter') {
+      console.log('tecla pressionada', event.key);
+    }
+  }
 
 }
